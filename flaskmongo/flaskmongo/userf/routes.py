09ff6b0d19bc3693c01ae2561eb3011c53414577
Subhajit Das <dassubhajit@gmail.com>
2023-09-15 21:11:17 +0530

    2nd commit

--- flaskmongo/flaskmongo/userf/routes.py
+++ flaskmongo/flaskmongo/userf/routes.py
@@ -1,75 +1,70 @@
 from flask import Blueprint,render_template,Response,request,jsonify
 from flaskmongo.models import User
+from flask.views import MethodView
+
 userf=Blueprint('userf',__name__)
 
-@userf.route('/')
-def abc():
-    return 'hello'
+class Userroute(MethodView):
+    def get(self,id=None):
+        if id is not None:
+            try: 
+                id=int(id)
+            except:
+                return jsonify({'message':f'id {id} is invalid'}),400
+            u=User.objects(uid=id).first()
+            if not u:
+                return jsonify({'message':f'id {id} not found'}),404
+            return jsonify(u)
+        u1=User.objects.all()
+        if not u1:
+            return jsonify({'message':f'user not found'}),404
+        return jsonify(u1)
+  
+    def post(self):
+        if request.method=='POST':
+            data=request.get_json()
+            id=int(data['id'])
+            name=data['name']
+            email=data['email']
+            password=data['password']
+            if id and name and email and password:
+                u=User.objects(uid=id).first()
+                if u:
+                    return Response('user already exist')
+                nu=User(uid=id,name=name,email=email,password=password)
+                nu.save()
+                return jsonify(nu),201
 
-
-@userf.route('/user',methods=['GET'])
-def home():
-    u=User.objects.all()
-    if not u:
-        return jsonify({'message':f'user not found'}),404
-    return jsonify(u)
-
-@userf.route('/user/<id>',methods=['GET'])
-def getuser(id):
-    try: 
-        id=int(id)
-    except:
-        return jsonify({'message':f'id {id} is invalid'}),400
-    u=User.objects(uid=id).first()
-    if not u:
-        return jsonify({'message':f'id {id} not found'}),404
-    return jsonify(u)
-    
-
-
-@userf.route('/user',methods=['POST'])
-def create_user():
-    if request.method=='POST':
+    def put(self,id):
+        try: 
+            id=int(id)
+        except:
+            return jsonify({'message':f'id {id} is invalid'}),400
+        u=User.objects(uid=id).first()
+        if not u:
+            return jsonify({'message':f'id {id} not found'}),404
         data=request.get_json()
-        id=data['id']
+        uid=data['id']
         name=data['name']
         email=data['email']
         password=data['password']
-        if id and name and email and password:
-            u=User.objects(uid=id).first()
-            if u:
-                return Response('user already exist')
-            nu=User(uid=id,name=name,email=email,password=password)
-            nu.save()
-            return jsonify(nu),201
+        u.update(uid=uid,name=name,email=email,password=password)
+        return jsonify({'message':'update successfuly'})
 
-@userf.route('/user/<id>',methods=['PUT'])
-def update_user(id):
-    try: 
-        id=int(id)
-    except:
-        return jsonify({'message':f'id {id} is invalid'}),400
-    u=User.objects(uid=id).first()
-    if not u:
-        return jsonify({'message':f'id {id} not found'}),404
-    data=request.get_json()
-    uid=data['id']
-    name=data['name']
-    email=data['email']
-    password=data['password']
-    u.update(uid=uid,name=name,email=email,password=password)
-    return jsonify({'message':'update successfuly'})
+    def delete(self,id):
+        try: 
+            id=int(id)
+        except:
+            return jsonify({'message':f'id {id} is invalid'}),400
+        u=User.objects(uid=id).first()
+        if not u:
+            return jsonify({'message':f'id {id} not found'}),404
+        u.delete()
+        return jsonify({'message':'delete successfuly'}),204
+    
 
-@userf.route('/user/<id>',methods=['DELETE'])
-def delete_user(id):
-    try: 
-        id=int(id)
-    except:
-        return jsonify({'message':f'id {id} is invalid'}),400
-    u=User.objects(uid=id).first()
-    if not u:
-        return jsonify({'message':f'id {id} not found'}),404
-    u.delete()
-    return jsonify({'message':'delete successfuly'}),204
+
+userf.add_url_rule('/user',view_func=Userroute.as_view('usercl'), methods=['GET', 'POST'])
+userf.add_url_rule('/user/<int:id>', view_func=Userroute.as_view('userpu'), methods=['GET', 'PUT', 'DELETE'])
 
 
